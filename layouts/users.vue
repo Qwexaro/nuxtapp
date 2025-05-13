56a195e8815e5933583f9c3790ca3e6e44cecf09
Qwexaro <Sergikstar25.gold@mail.ru>
2025-05-13 23:49:35 +0300

    make cards of users

---
+++ layouts/users.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <header class="bg-dark p-3">
+        <ul class="nav container">
+            <li class="nav-item"><NuxtLink to="/" class="nav-link text-white">Main</NuxtLink></li>
+            <li class="nav-item"><NuxtLink to="/contact" class="nav-link text-white">Contacts</NuxtLink></li>
+            <li class="nav-item"><NuxtLink to="/users/" class="nav-link text-white">Users</NuxtLink></li>
+        </ul>
+    </header>
+    
+    <div class="container py-5">
+    <slot></slot>
+    </div>
+    
+    <footer class="bg-dark p-3">
+        <p class="text-white container">All rights reserved by Qwexaro&copy;</p>
+    </footer>
+
+</div>
+</template>
+
+<style>
+
+header a:not(.router-link-exact-active):hover{
+    text-decoration: underline;
+}
+
+header a.router-link-exact-active{
+    color: #ffb636!important;
+    font-weight: 500;
+}
+
+h1{color: red}
+
+</style>
---
+++ pages/users/add.vue
@@ -0,0 +1,15 @@
+<script>
+definePageMeta({
+    layout: 'users'
+})
+</script>
+
+<template>
+  <div>
+    <h1>Add users</h1>
+  </div>
+</template>
+
+<style>
+h1{color: red}
+</style>
