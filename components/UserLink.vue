e86891dd5846a4c1f0a0e804533d952bfb72a503
Qwexaro <SergeyIvanovWork47@gmail.com>
2026-08-17 20:33:21 +0300

    updated UserLink

--- components/UserLink.vue
+++ components/UserLink.vue
@@ -8,14 +8,16 @@ defineProps({
 </script>
 
 <template>
-  <div class="card border border-orange-500 p-2 h-100 shadow-sm
-              bg-gradient-to-r from-white to-orange-100 rounded-lg">
+  <div class="card border border-orange-500 p-2 h-100 shadow-sm bg-gradient-to-r from-white to-orange-100 rounded-lg">
     <img :src="user.image" class="card-img-top w-full h-48 object-cover rounded-t-lg" alt="User Image">
     <div class="card-body p-3">
       <h5 class="card-title text-lg font-semibold">{{ user.firstName }} {{ user.lastName }}</h5>
       <p class="card-text text-muted text-sm">Age: {{ user.age }} | {{ user.gender }}</p>
       <p class="text-sm"><strong>Email: </strong>{{ user.email }}</p>
-      <NuxtLink :to="`/users/user-${user.id}`" class="btn btn-primary inline-block mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors">
+      <NuxtLink 
+        :to="`/users/user-${user.id}`" 
+        class="btn btn-primary inline-block mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
+      >
         View Profile
       </NuxtLink>
     </div>
