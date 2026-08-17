ac7a9c3dd0c74f7a857e36bab972d4c8a8715b85
Qwexaro <SergeyIvanovWork47@gmail.com>
2026-08-17 20:03:13 +0300

    updated nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,28 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/css/tailwind.css'],
+  
+  tailwindcss: {
+    viewer: true,
+    config: {
+      content: [
+        './components/**/*.{vue,js,ts}',
+        './layouts/**/*.vue',
+        './pages/**/*.vue',
+        './app.vue',
+        './error.vue',
+        './plugins/**/*.{js,ts}'
+      ]
+    }
+  },
+  
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/css/tailwind.css" as *;'
+        }
+      }
+    }
+  }
 })
